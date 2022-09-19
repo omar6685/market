90f6d1adf807a0288d8b879ef55a617493e40c6f
omar6685 <omr6685@hotmail.com>
2022-09-19 17:00:44 +0300

    deploy to heroku

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,22 @@
 class CheckoutController < ApplicationController
 
+    before_action :allow_cross_domain_access
+    after_action :cors_set_access_control_headers
+    
+    def allow_cross_domain_access
+        headers['Access-Control-Allow-Origin'] = '*'# http://localhost:9000
+        headers['Access-Control-Allow-Headers'] = '*'
+        headers['Access-Control-Allow-Methods'] = '*'
+        headers['Access-Control-Allow-Credentials'] = 'true'
+        headers['Access-Control-Max-Age'] = '1728000'
+    end
+    def cors_set_access_control_headers
+            headers['Access-Control-Allow-Origin'] = '*'
+            headers['Access-Control-Allow-Methods'] = '*'
+            headers['Access-Control-Allow-Headers'] = '*'
+            headers['Access-Control-Allow-Credentials'] = 'true'
+            headers['Access-Control-Max-Age'] = "1728000"
+    end
 
     def create 
         product = Product.find(params[:id])
